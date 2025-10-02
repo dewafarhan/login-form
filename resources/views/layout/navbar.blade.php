aa39470040b2745a3f08e24e219aaa71d34c90f4
Dewa Alyanida Farhan <dewafarhan@gmail.com>
2025-10-02 11:13:42 +0700

    update home

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -5,7 +5,11 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#"class="nav-link">Home</a>
+       @if(auth()->user()->role == 'staff')
+            <a href="{{ url('/staff') }}" class="nav-link">Home</a>
+        @else
+            <a href="{{ url('/dashboard') }}" class="nav-link">Home</a>
+        @endif
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="{{ route('contact') }}" class="nav-link">Contact</a>
